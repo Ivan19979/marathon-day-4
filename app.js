e1a07bc1569264023317c30c4bb5a3ef41e74299
Ivan Shchururpov <vanya.schurupov@yandex.ru>
2023-09-05 12:48:00 +0300

    marathon

---
+++ app.js
@@ -0,0 +1,34 @@
+const board = document.querySelector('#board');
+const colors = ['red', 'blue', 'green', 'yellow', 'purple'];
+const SQUARES_NUMBER = 500;
+
+for(let i = 0; i < SQUARES_NUMBER; i++){
+    const square = document.createElement('div');
+    square.classList.add('square');
+
+    square.addEventListener('mouseover', () => {
+        setColor(square);
+    });
+    square.addEventListener('mouseleave', () => {
+        removeColor(square);
+    });
+    board.append(square);
+}
+
+function setColor(square) {
+    const color = getRandomColor();
+    square.style.backgroundColor = color;
+    square.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+    return square;
+}
+
+function removeColor(square) {
+    square.style.backgroundColor = '#1d1d1d';
+    square.style.boxShadow = '0 0 2px #000';
+    return square;
+}
+
+function getRandomColor(){
+    const index = Math.floor(Math.random() * colors.length);
+    return colors[index];
+}
